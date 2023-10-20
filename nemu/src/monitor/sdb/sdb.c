462f8fd8d51cbcbaa7ae7607c6d9b8e35081d53b
tracer-ysyx <tracer@ysyx.org>
2023-10-20 12:09:44 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:09:44 up 1 day, 23:16,  1 user,  load average: 0.05, 0.09, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -114,7 +114,7 @@ static int cmd_p(char *args) {
   else {
     bool *success = NULL;
     word_t result = expr(args, success);
-    if(*success)
+    if(*(success)==true)
       printf("$1 = %d", result);
     return *success;
   }

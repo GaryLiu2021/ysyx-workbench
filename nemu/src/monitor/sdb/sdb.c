6699f673be6a79545ebf2cf40221564ced1e9504
tracer-ysyx <tracer@ysyx.org>
2024-03-05 12:57:34 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:57:34 up  2:47,  1 user,  load average: 0.08, 0.05, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -101,7 +101,7 @@ static int cmd_x(char* args) {
 			printf("Steps: %d\n", steps);
 			printf("Address: 0x%x\n", address);
 			for (int i = 0; i < steps; i++) {
-				printf("%x: %0x\n", address + i, vaddr_ifetch(address + i, 4));
+				printf("%x: %08x\n", address + i, vaddr_ifetch(address + i, 4));
 			}
 			return 0;
 		}

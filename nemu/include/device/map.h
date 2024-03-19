0e9a30cd7494065c2a25ffc8920d07989b0f71d3
tracer-ysyx <tracer@ysyx.org>
2024-03-19 10:46:44 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:46:44 up 14 days, 35 min,  1 user,  load average: 0.08, 0.11, 0.08

--- nemu/include/device/map.h
+++ nemu/include/device/map.h
@@ -45,12 +45,14 @@ static inline int find_mapid_by_addr(IOMap *maps, int size, paddr_t addr) {
   return -1;
 }
 
+// Register a port-IO mapping
 void add_pio_map(const char *name, ioaddr_t addr,
         void *space, uint32_t len, io_callback_t callback);
 void add_mmio_map(const char *name, paddr_t addr,
         void *space, uint32_t len, io_callback_t callback);
 
-word_t map_read(paddr_t addr, int len, IOMap *map);
+// API to access the I/O space
+word_t map_read(paddr_t addr, int len, IOMap* map);
 void map_write(paddr_t addr, int len, word_t data, IOMap *map);
 
 #endif

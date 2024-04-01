7cc7946e9e48833ac0900073f9505612dbe7910c
tracer-ysyx <tracer@ysyx.org>
2024-04-01 22:14:25 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:25 up 2 days,  6:33,  1 user,  load average: 0.03, 0.04, 0.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -50,58 +50,78 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	return vsnprintf(out, -1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
+	va_list arg;
+	va_start(arg, fmt);
 
-    int written = 0;  // 记录写入的字符数
-    char buf[256];
+	int res = vsprintf(out, fmt, arg);    // 将格式化的内容(字符串)赋值给out
 
-    while (*fmt) {
-        if (*fmt != '%') {
-            // 普通字符，直接复制到缓冲区
-            buf[written++] = *fmt++;
-        } else {
-            // 遇到 '%'，处理格式化符号
-            fmt++;  // 跳过 '%'
-            if (*fmt == '\0') {
-                break;  // 避免在字符串末尾的 '%' 导致无限循环
-            }
-
-            if (*fmt == 'd') {
-                // 处理整数格式化符号 %d
-                int num = va_arg(args, int);
-                char str[32] = {};
-                itoa(num, str, 10);
-                int i = 0;
-                while (str[i]!='\0') {
-                  buf[written++] = str[i++];
-                }
-            } else if (*fmt == 's') {
-                // 处理字符串格式化符号 %s
-                const char *str = va_arg(args, const char *);
-                while (*str) {
-                    buf[written++] = *str++;
-                }
-            }
-            fmt++;  // 移动到下一个字符
-        }
-    }
-    va_end(args);
-    buf[written] = '\0'; // 添加 null 终止符
-    strcpy(out, buf);
-    return written;
+	va_end(arg);
+	return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
-
+static char NUM_CHAR[] = "0123456789ABCDEF";
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int len = 0;
+	char buf[128];
+	int buf_len = 0;
+	while (*fmt != '\0' && len < n) {
+		switch (*fmt) {
+		case '%':
+			fmt++;
+			// 检查百分号之后的字符
+			switch (*fmt) {
+			case 'd':
+				int val = va_arg(ap, int);    // 将该参数转为int型
+				if (val == 0) out[len++] = '0';
+				if (val < 0) {
+					out[len++] = '-';
+					val = 0 - val;
+				}
+				for (buf_len = 0; val;val /= 10, buf_len++)
+					buf[buf_len] = NUM_CHAR[val % 10];    //这里buf会是逆序的
+				for (int i = buf_len - 1; i >= 0; i--)
+					out[len++] = buf[i];
+				break;
+			case 'u':
+				// uint32_t uval = va_arg(ap, uint32_t);
+				// 同%d, 只不过不用考虑负数
+				break;
+			case 'c':
+				char c = (char)va_arg(ap, int);    //va_arg函数没有char这个参数
+				out[len++] = c;
+				break;
+			case 's':
+				char* s = va_arg(ap, char*);
+				for (int i = 0; s[i] != '\0'; i++)
+					out[len++] = s[i];
+				break;
+			case 'p':
+				out[len++] = '0'; out[len++] = 'x';
+				uint32_t address = va_arg(ap, uint32_t);
+				for (buf_len = 0; address; address /= 16, buf_len++)
+					buf[buf_len] = NUM_CHAR[address % 16];
+				for (int i = buf_len - 1; i >= 0; i--)
+					out[len++] = buf[i];
+				break;
+			}
+			break; // case % 的break.
+		case '\n':
+			out[len++] = '\n';
+			break;
+		default:
+			out[len++] = *fmt;
+		}
+		fmt++;
+	}
+	out[len] = '\0';    // 这句千万不能漏.
+	return len;
 }
 
 #endif

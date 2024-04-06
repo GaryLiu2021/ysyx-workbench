865a4a3f2bee8c4c75f4dc06b234a041007facc5
tracer-ysyx <tracer@ysyx.org>
2024-04-06 16:05:58 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:05:58 up 7 days, 24 min,  1 user,  load average: 0.23, 0.14, 0.09

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -126,6 +126,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, npc);
+  isa_reg_display();
 }
 #else
 void init_difftest(char *ref_so_file, long img_size, int port) { }

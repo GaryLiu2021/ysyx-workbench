430f5563bf5c0a1c0d4ffac424e8d87bcfab10f7
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:06:08 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:08 up 6 days,  5:15,  1 user,  load average: 0.01, 0.04, 0.03

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@ extern void do_syscall(Context* c);
 static Context* do_event(Event e, Context* c) {
 	switch (e.event) {
 	case(EVENT_SYSCALL):do_syscall(c); break;
+	case(EVENT_YIELD):;
 	default: panic("Unhandled event ID = %d", e.event);
   }
 

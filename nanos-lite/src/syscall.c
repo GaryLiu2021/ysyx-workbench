6b31effd41b6a61f7b31cdaac6822a9772000979
tracer-ysyx <tracer@ysyx.org>
2024-04-15 20:56:39 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:56:39 up 7 days,  5:05,  1 user,  load average: 0.21, 0.11, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,7 +20,6 @@ void do_syscall(Context* c) {
 	}
 	case SYS_write: {
 		int fd = c->GPR2;
-		Log("fd=%d", fd);
 		void* buf = (void*)c->GPR3;
 		unsigned int count = c->GPR4;
 		Log("Nanos-lite: Doing syscall _write(int fd=%d, void *buf=%p, size_t count=%d)", fd, buf, count);

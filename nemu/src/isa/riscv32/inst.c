574a8687c56a7e2d9e768307d895acdc99abf415
tracer-ysyx <tracer@ysyx.org>
2024-04-15 13:24:56 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:24:56 up 6 days, 21:34,  1 user,  load average: 0.03, 0.05, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -82,7 +82,7 @@ static int decode_exec(Decode* s) {
 		{branch_cnt=0;} 
 
 	// #define ECALL(dnpc) { bool success; dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc)); }
-#define ECALL(dnpc) {dnpc = (isa_raise_intr(0xb, s->pc)); Log(ANSI_FMT("ETRACE: expection called @ 0x%08x, going to 0x%08x...\n",ANSI_FG_BLUE),s->pc,dnpc);}
+#define ECALL(dnpc) {bool success; dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc)); Log(ANSI_FMT("ETRACE: expection called @ 0x%08x, going to 0x%08x...\n",ANSI_FG_BLUE),s->pc,dnpc);} // Note that mcause in qemu may be 0xb forever.
 #define CSR(i) *get_csr_register(i)
 
 	/* INTPAT Begin */

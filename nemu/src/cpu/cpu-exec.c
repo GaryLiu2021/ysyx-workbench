7d83a7254a42f53b90e4df493aa385da92c1bce6
tracer-ysyx <tracer@ysyx.org>
2024-04-17 14:27:41 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:41 up 8 days, 22:36,  1 user,  load average: 0.41, 0.46, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,7 +43,8 @@ void nmu_ringbuf_free(NMU_RINGBUF* rb);
 
 static void trace_and_difftest(Decode* _this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
-	if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+	// if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+	log_write("%s\n", _this->logbuf);
 #endif
 	// if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
 	IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));

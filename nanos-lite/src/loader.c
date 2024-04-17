f5ac5f79c2acc9c5024ee50017ef881d7fdbc90d
tracer-ysyx <tracer@ysyx.org>
2024-04-17 13:04:42 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:04:42 up 8 days, 21:13,  1 user,  load average: 0.07, 0.08, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,16 +12,21 @@
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 	int fd = fs_open(filename, 0, 0);
+	assert(fd >= 0);
+
 	Elf_Ehdr ehdr;
-	fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+	size_t bytes_read = fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+	assert(bytes_read == sizeof(Elf_Ehdr));
 	// ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
 	// check valid elf
 	assert((*(uint32_t*)ehdr.e_ident == 0x464c457f));
 
 	Elf_Phdr phdr[ehdr.e_phnum];
 	fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
-	fs_read(fd, phdr, sizeof(Elf_Phdr) * ehdr.e_phnum);
+	bytes_read = fs_read(fd, phdr, sizeof(Elf_Phdr) * ehdr.e_phnum);
+	assert(bytes_read == sizeof(Elf_Phdr) * ehdr.e_phnum);
 	// ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
+
 	for (int i = 0; i < ehdr.e_phnum; i++) {
 		if (phdr[i].p_type == PT_LOAD) {
 			fs_read(fd, (void*)phdr[i].p_vaddr, phdr[i].p_memsz);

3baad7651e22547c2e2c803780431a03e758726b
tracer-ysyx <tracer@ysyx.org>
2024-04-18 21:39:58 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:39:58 up 10 days,  5:48,  1 user,  load average: 5.90, 5.93, 5.18

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -22,6 +22,7 @@ int SDL_PollEvent(SDL_Event* ev) {
 	for (int i = 0; i < 83; i++) {
 		printf("FUCK %c\n", buf[3]);
 		printf("FUCKa %s\n", keyname[i]);
+		printf("%d %d\n", strlen(keyname[i]), strlen(buf + 3));
 		printf("%d\n", strcmp(keyname[i], buf + 3));
 		if (strcmp(keyname[i], buf + 3) == 0) {
 			printf("FUCK1\n");

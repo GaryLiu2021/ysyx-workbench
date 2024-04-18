af8abc15d5cf768d284763bcb9f269fd0acdb37f
tracer-ysyx <tracer@ysyx.org>
2024-04-18 16:19:24 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:19:24 up 10 days, 28 min,  1 user,  load average: 0.82, 0.73, 0.71

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -12,7 +12,7 @@ int main() {
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   printf("%d,%d\n", w, h);
-  NDL_DrawRect(bmp, 0, 0, w, h);
+  NDL_DrawRect(bmp, 136, 86, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");

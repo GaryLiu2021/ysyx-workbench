94b0d1b3c09aecf82c60bcb7f4117f3dada62769
tracer-ysyx <tracer@ysyx.org>
2024-04-18 12:00:07 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:00:07 up 9 days, 20:08,  1 user,  load average: 0.22, 0.18, 0.18

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -12,8 +12,8 @@ int main() {
 		while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
 			gettimeofday(&tv, NULL);
 		}
-		printf("ms = %d\n", ms);
+		printf("ms = %x\n", ms);
 		ms += 500;
-		printf("ms = %d\n", ms);
+		printf("ms = %x\n", ms);
 	}
 }
